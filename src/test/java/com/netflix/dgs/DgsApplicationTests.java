5ab136ee7d1ed758d9f04e2bf489c2779d553934
unknown <danilo.santos@elotech.com.br>
2021-05-27 06:46:07 -0300

    init

---
+++ src/test/java/com/netflix/dgs/DgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.dgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
